8901e885f01bbc5747ccf1abe32073ddaf028754
Traian Nedelea <tron1point0@gmail.com>
2012-06-16 10:55:54 -0500

    Different start script

--- server.sh
+++ server.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-plackup -s ${STATIC_SERVER:-Twiggy} --port ${STATIC_PORT:-5000} app.psgi &> static.log &
-plackup -s ${ASYNC_SERVER:-Twiggy} --port ${ASYNC_PORT:-5001} chat.pl &> async.log &
+(
+    plackup \
+        -s ${STATIC_SERVER:-Twiggy} \
+        --port ${STATIC_PORT:-5000} \
+        app.psgi &
+    echo "$!" > .static.pid
+    plackup \
+        -s ${ASYNC_SERVER:-Twiggy} \
+        --port ${ASYNC_PORT:-5001} \
+        chat.pl &
+    echo "$!" > .async.pid
+) | tee access.log
+
+kill $(cat .static.pid)
+kill $(cat .async.pid)
+rm .static.pid .async.pid
